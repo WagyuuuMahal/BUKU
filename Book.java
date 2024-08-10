a4b72944828c92a4b562c2ddf8ddea0933369eba
Duhh wayuuu ★ <wahyu_anggoro_33rpl@student.smktelkom-mlg.sch.id>
2024-08-10 21:09:46 +0700

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,39 @@
+public class Book {
+        public static void main(String[] args) {
+            //object dengan constructor parameter
+            TestBuku wayuu = new TestBuku("Malioboro at Midight", 421, "Skysphire", 2023);
+            System.out.println("Wayuu Meminjam Buku : ");
+            System.out.println("Judul = "+wayuu.Judul);
+            System.out.println("Id = "+wayuu.id);
+            System.out.println("pengarang = "+wayuu.Pengarang);
+            System.out.println("Tahun Terbit = "+wayuu.TahunTerbit);
+            TestBuku Felli = new TestBuku("Garis waktu ",   212, "Fiersa besari", 2016 );
+            System.out.println("Felli meminjam buku : ");
+            System.out.println("Judul = "+Felli.Judul);
+            System.out.println("Id = "+Felli.id);
+            System.out.println("pengarang = "+Felli.Pengarang);
+            System.out.println("Tahun Terbit = "+Felli.TahunTerbit);
+            TestBuku didaq = new TestBuku("Konspirasi Alam Semesta ", 244, "Fiersa besari", 2017 );
+            System.out.println("Didaq meminjam buku : ");
+            System.out.println("Judul = "+didaq.Judul);
+            System.out.println("Id = "+didaq.id);
+            System.out.println("pengarang = "+didaq.Pengarang);
+            System.out.println("Tahun Terbit = "+didaq.TahunTerbit);
+            TestBuku mozaa = new TestBuku("Catatan Juang",306, "Fiersa besari", 2017 );
+            System.out.println("Mozaa meminjam buku : ");
+            System.out.println("Judul = "+mozaa.Judul);
+            System.out.println("Id = "+mozaa.id);
+            System.out.println("pengarang = "+mozaa.Pengarang);
+            System.out.println("Tahun Terbit = "+mozaa.TahunTerbit);
+            TestBuku Claraa = new TestBuku("Tapak jejak", 312, "Fiersa besari", 2019 );
+            System.out.println("Claraa meminjam buku : ");
+            System.out.println("Judul = "+Claraa.Judul);
+            System.out.println("Id = "+Claraa.id);
+            System.out.println("pengarang = "+Claraa.Pengarang);
+            System.out.println("Tahun Terbit = "+Claraa.TahunTerbit);
+    
+        
+    
+    }    
+    
+}
---
+++ TestBuku.java
@@ -0,0 +1,15 @@
+public class TestBuku {
+    String Judul;
+    int id;
+    String Pengarang;
+    int TahunTerbit;
+
+    //constructor parameter
+    public TestBuku(String Judul, int id, String Pengarang, int TahunTerbit){
+        this.Judul = Judul;
+        this.id = id;
+        this.Pengarang = Pengarang;
+        this.TahunTerbit = TahunTerbit;
+    }
+    
+}
